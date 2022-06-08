00042c61e184d84f045ada9a50bde20f766e9674
CarlosVlaCor <valenzuelacorralescarlos@gmail.com>
2022-06-08 17:02:32 -0600

    Creado proyecto de donacion de sangre MISAN con Spring Boot

---
+++ MISAN/src/main/java/com/misan/donacionSangre/MisanApplication.java
@@ -0,0 +1,13 @@
+package com.misan.donacionSangre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisanApplication.class, args);
+	}
+
+}
---
+++ MISAN/src/test/java/com/misan/donacionSangre/MisanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misan.donacionSangre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
